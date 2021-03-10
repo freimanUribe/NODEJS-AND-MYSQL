82b1a59e0ba4f84d79136469f4705054d2e3781b
freimanUribe <61211113+freimanUribe@users.noreply.github.com>
2021-03-10 08:35:36 -0500

    commit 2

--- componets/message/network.js
+++ componets/message/network.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
 //Importa respuestas del response
 const response = require('../../network/response');
@@ -8,7 +8,7 @@ const response = require('../../network/response');
 const controller = require('./controller');
 
 //importacion de la llave secreta
-const keys = require('../../settings/key').key;
+// const keys = require('../../settings/key').key;
 
 const router = express.Router();
 
@@ -36,57 +36,57 @@ router.post('/a', function (req, res) {
 
 });
 
-router.post("/", (req , res) => {
-console.log(req.body.user, req.body.password);
-    controller.search(req.body.user, req.body.password)
-    .then((fullMessage)=>{
+// router.post("/", (req , res) => {
+// console.log(req.body.user, req.body.password);
+//     controller.search(req.body.user, req.body.password)
+//     .then((fullMessage)=>{
 
-        const payload = {
-            check:true
-        };
-        const jj = jwt.sign(payload, keys, {
-            expiresIn: '0.5h'
-        });
-        console.log(jj);
-        response.success(req, res, fullMessage, 201);
-    })
-    .catch(e=>{
-        response.error(req, res, 'Error informacion invalidad', 400, 'este mensaje es para desarrollador');
-    });
-});
+//         const payload = {
+//             check:true
+//         };
+//         const jj = jwt.sign(payload, keys, {
+//             expiresIn: '0.5h'
+//         });
+//         console.log(jj);
+//         response.success(req, res, fullMessage, 201);
+//     })
+//     .catch(e=>{
+//         response.error(req, res, 'Error informacion invalidad', 400, 'este mensaje es para desarrollador');
+//     });
+// });
 
-router.post("/posts", verifyToken, (req , res) => {
-    res.json('informacion protegita vista');
-});
+// router.post("/posts", verifyToken, (req , res) => {
+//     res.json('informacion protegita vista');
+// });
 
-function verifyToken(req, res, next){
+// function verifyToken(req, res, next){
 
-    let tokens = req.headers['x-access-token'] || req.headers['authorization'];
+//     let tokens = req.headers['x-access-token'] || req.headers['authorization'];
     
-    if (!tokens) {
-        res.status(401).send({
-            error:'es necesario el token',
-        });
-        return
-    }
+//     if (!tokens) {
+//         res.status(401).send({
+//             error:'es necesario el token',
+//         });
+//         return
+//     }
     
-    if (tokens.startsWith('Bearer ')) {
-        tokens = tokens.slice(7, tokens.length);
-    }
+//     if (tokens.startsWith('Bearer ')) {
+//         tokens = tokens.slice(7, tokens.length);
+//     }
     
-    if (tokens) {
-        jwt.verify(tokens, keys, (error, decoded)=>{
-            if (error) {
-                return res.json({
-                    message:'token no valido',
-                });
-            }else{ 
-                req.decoded = decoded+"11";
-                next();
-            }
+//     if (tokens) {
+//         jwt.verify(tokens, keys, (error, decoded)=>{
+//             if (error) {
+//                 return res.json({
+//                     message:'token no valido',
+//                 });
+//             }else{ 
+//                 req.decoded = decoded+"11";
+//                 next();
+//             }
            
-        });
-    }
-}
+//         });
+//     }
+// }
 
 module.exports = router;
